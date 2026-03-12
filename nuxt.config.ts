0b0bbf87cf7dff378373bf5bc169aa9bc540007c
Promise <i@xenlyra.cc>
2026-03-12 19:22:45 +0800

    introduce vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
       }
     }
   },
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@vueuse/nuxt'],
   routeRules: { '/': { prerender: true } },
   ssr: false
 })
