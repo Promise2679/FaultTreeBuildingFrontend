87c7c5c83c946757a8dc362169ee41dcc709f000
Promise <i@xenlyra.cc>
2026-04-20 13:20:12 +0800

    refactor

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,10 @@
+<script setup lang="ts">
+const { isSidebarOpen } = useFaultTree()
+</script>
+
 <template>
   <AppHeader />
   <ChatSidebar />
   <FaultTreeCanvas class="fixed inset-0" />
   <NodeEditSidebar v-if="isSidebarOpen" />
 </template>
-
-<script setup lang="ts">
-const { isSidebarOpen } = useFaultTree()
-</script>
